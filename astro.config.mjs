0af1e290e01d707fedd172d5dcfdf03dd298aa0a
dialecticalpraxis <github.lesser968@passmail.net>
2026-06-25 15:05:14 -0500

    completed free-school project write-up

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,7 +37,7 @@ const viteConfig = {
 export default defineConfig({
   compressHTML: true,
   site: 'https://www.dialecticalpraxis.com',
-  integrations: [compress(), icon(), mdx(), sitemap()],
+  integrations: [compress({ Image: false }), icon(), mdx(), sitemap()],
   vite: viteConfig,
   env: {
     schema: {
